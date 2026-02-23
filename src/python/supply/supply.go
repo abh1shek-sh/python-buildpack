5d96a36945ec57210c1b23b0c76f07a5359d7843
Abhishek Sharma <76648011+abh1shek-sh@users.noreply.github.com>
2026-02-23 18:13:53 +0530

    Change flit_core installation to flit
    Replaced installation of flit_core with flit for building wheels.

--- src/python/supply/supply.go
+++ src/python/supply/supply.go
@@ -748,7 +748,7 @@ func (s *Supplier) InstallCommonBuildDependencies() error {
 	// Install flit_core first from PyPI (needed to build wheel >= 0.40)
 	// flit_core is a pure Python wheel with no build dependencies
 	s.Log.Info("Installing flit_core from PyPI (required to build wheel)")
-	if err := s.runPipInstall("flit_core>=3.8,<4"); err != nil {
+	if err := s.runPipInstall("flit"); err != nil {
 		s.Log.Warning("Could not install flit_core from PyPI: %v. Continuing anyway...", err)
 	}
 
